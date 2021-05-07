4081190a5be34feedb19fcd889f4e4cc9f17b168
rusualexrsa <rusualexrsa@gmail.com>
2021-05-07 16:06:53 +0300

    Added CORS

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
-
-import * as logger from 'koa-logger';
-import * as json from 'koa-json';
+import * as cors from '@koa/cors';
 import { walletRouter } from './dependecies';
 
 const app = new Koa();
 const router = new Router().use(walletRouter.getRouter().routes());
 
-app.use(json());
-app.use(logger());
-
+app.use(cors());
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(process.env.PORT || 3000, () => {
